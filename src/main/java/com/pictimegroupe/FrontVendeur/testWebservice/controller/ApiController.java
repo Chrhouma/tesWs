96ab2074f8fabee93a343bcea09616d47b12e670
RhoumaC <Charfeddine.Rhouma@pictime-groupe.com>
2018-06-29 17:41:34 +0200

    lot1

--- src/main/java/com/pictimegroupe/FrontVendeur/testWebservice/controller/ApiController.java
+++ src/main/java/com/pictimegroupe/FrontVendeur/testWebservice/controller/ApiController.java
@@ -34,6 +34,7 @@ public class ApiController {
     DeltaServices deltaServices;
     @Autowired
     DeltaRepository deltaRepository;
+    //
    private String baseURI = "http://127.0.0.1/";
     /**
      * @param login
